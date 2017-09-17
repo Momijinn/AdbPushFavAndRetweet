38ed217ed3f5a4b9afea52c6129079a3f1e803a0
Kana <momijinn.aka@gmail.com>
2017-09-18 08:44:11 +0900

    initialize commit

---
+++ AdbPushFavAndRetweet.js
@@ -0,0 +1,116 @@
+//Twitter
+var Twitter = require('twitter');
+var client = new Twitter({
+ consumer_key: '',
+ consumer_secret: '',
+ access_token_key: '',
+ access_token_secret: ''
+});
+
+//AmazonDashButton
+var dash_button = require('node-dash-button');
+var dash_ip = 'YOUR_DASH_BUTTON_IP';
+
+//adb recv
+const dash = dash_button(dash_ip, null, null, 'all');
+dash.on('detected', () => {
+    AutoFollow(client, 'TWITTER_ID');
+    GetHomeTimeLine(client);
+
+});
+
+
+//HomeTimeLineのフォローとリツイート
+function GetHomeTimeLine(client){
+    client.get('statuses/home_timeline', function(error, tweets, response) {
+        if (!error) {
+            Object.keys(tweets).forEach(function(key) {
+            var val = this[key];
+            var obj = (key, val);
+
+            var Id = obj['id_str'];
+            var Text = obj['text'];
+            var Favorited = obj['favorited'];
+            var Retweeted = obj['retweeted'];
+
+
+            if (!Text.match(/@/i)) { //リツイートのものは除去
+                //お気に入り
+                if(Favorited == false){
+                    client.post('favorites/create', {id: Id}, function(error, response){
+                        if(error) return;
+                        else console.log('favorited!!');
+                    });
+                }
+                //リツイート
+                if(Retweeted == false){
+                    client.post('statuses/retweet', {id: Id}, function(error, response){
+                        if(error) return;
+                        else console.log('Retweeted!!');
+                    });
+                }
+            }
+          }, tweets);
+        }
+    });
+}
+
+
+//ユーザータイムラインからのフォローとリツイート
+function GetUserTimeLine(client, screenname){
+    var params = {screen_name: screenname};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+          Object.keys(tweets).forEach(function(key) {
+            var val = this[key];
+            var obj = (key, val);
+
+            var Id = obj['id_str'];
+            var Text = obj['text'];
+            var Favorited = obj['favorited'];
+            var Retweeted = obj['retweeted'];
+
+
+            if (!Text.match(/@/i)) { //リツイートのものは除去
+                //お気に入り
+                if(Favorited == false){
+                    client.post('favorites/create', {id: Id}, function(error, response){
+                        if(error) return;
+                        else console.log('favorited!!');
+                    });
+                }
+                //リツイート
+                if(Retweeted == false){
+                    client.post('statuses/retweet', {id: Id}, function(error, response){
+                        if(error) return;
+                        else console.log('Retweeted!!');
+                    });
+                }
+            }
+          }, tweets);
+        }
+    });
+}
+
+
+//自動フォロー
+function AutoFollow(client, screenname){
+    var params = {screen_name: screenname};
+    client.get('followers/list', params, function(error, profiles, response) {
+        if (!error) {
+            var profile = profiles['users'];
+
+            profile.forEach(function(obj){
+                var Id = obj['id_str'];
+                var ScreenName = obj['screen_name'];
+                var Following = obj['following'];
+
+                if(Following == false){
+                    client.post('friendships/create', {user_id: Id}, function(error, response){
+                        if(error) return;
+                    });
+                }
+            });
+        }
+    });
+}
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # AdbPushFavAndRetweet
+AmazonDashButton(ADB)が押されるとTwitterにてお気に入りとリツイートがされるプログラム
+
+## 確認動作環境
+* Nodejs v4.2.6
+
+
+## 必要なモジュール
+```bash
+npm install twitter
+npm install node-dash-button
+```
+
+## プログラム
+AdbPushFavAndRetweet.jsについて
+
+※TwitterのToken等とADBのIPアドレスが入手できている前提で記述します
+
+```javascript
+//Twitterのkey,secret,tokenを記述
+var client = new Twitter({
+ consumer_key: '',
+ consumer_secret: '',
+ access_token_key: '',
+ access_token_secret: ''
+});
+
+/////中略/////
+
+//ADBのIPを記述
+var dash_ip = 'YOUR_DASH_BUTTON_IP';
+
+/////中略/////
+
+//ADBのcall
+const dash = dash_button(dash_ip, null, null, 'all');
+dash.on('detected', () => {
+    //フォロワーの取得 フォロワーを取得したいTwitterID記述
+    AutoFollow(client, 'TWITTER_ID');
+
+    //リツイートとお気に入り
+    GetHomeTimeLine(client);
+
+});
+
+```
